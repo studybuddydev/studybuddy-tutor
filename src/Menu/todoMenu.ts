a4e29a355143e806e04784f4cf0a6dc7dc87ac4f
alessiogandelli <alessiogandelli99@gmail.com>
2024-03-21 16:47:23 +0100

    todo menu

--- src/Menu/todoMenu.ts
+++ src/Menu/todoMenu.ts
@@ -4,53 +4,77 @@ import logger from 'euberlog';
 // settingsmenu used to handle notification of the bot
 
 
+async function editMsgDelete(ctx: MyContext) {
+
+    if (!ctx.from) return
+
+
+
+    //get current message
+    let msg = 'hai ' + ctx.session.todo?.length + ' cose da fare: \n'
+    ctx.session.todo?.forEach((todo, index) => {
+        msg += `\n${index + 1}. ${todo}`;
+    });
+
+    try {
+        await ctx.editMessageText(msg + '\n\n seleziona il todo da eliminare');
+    } catch (e) {
+        logger.warning(e as string);
+    }
+}
+
 export const todoMenu = new Menu<MyContext>("todo-menu")
-  .text(
-    "add todo",
-    (ctx) => {
-      //edit message 
-      //ctx.session.todo?.push(ctx.message?.text || '');
-     //   }catch (e) { console.log(e)} 
-      ctx.reply('/todo todo da aggiungere')
-    },
-  ).submenu("elimina", 'delete-todo').row() 
+    .text(
+        "add todo",
+        (ctx) => {
+            //edit message 
+            //ctx.session.todo?.push(ctx.message?.text || '');
+            //   }catch (e) { console.log(e)} 
+            ctx.reply('/todo todo da aggiungere')
+        },
+    ).submenu("elimina", 'delete-todo', editMsgDelete).row()
 
 const addTodoMenu = new Menu<MyContext>("add-todo")
 
-  .text( 
-    "aggiungi",
-    async (ctx) => {
-      await ctx.session.todo?.push(ctx.message?.text || '');
-    }
-  ).row()
-  .back("Go Back");
+    .text(
+        "aggiungi",
+        async (ctx) => {
+            await ctx.session.todo?.push(ctx.message?.text || '');
+        }
+    ).row()
+    .back("Go Back");
 
 
 const deleteTodoMenu = new Menu<MyContext>("delete-todo")
-  .dynamic(async (ctx) => {
-    // Generate a part of the menu dynamically!
-    const range = new MenuRange<MyContext>();
+    .dynamic(async (ctx) => {
+        // Generate a part of the menu dynamically!
+        const range = new MenuRange<MyContext>();
 
-    ctx.session.todo?.forEach((todo, index) => {
-      range
-        .text((todo).toString(), (ctx) => {
-          ctx.session.todo?.splice(index, 1) 
-          ctx.menu.update();
-
-          let msg = 'hai ' + ctx.session.todo?.length + ' cose da fare: \n'
-          ctx.session.todo?.forEach((todo, index) => {
-              msg += `\n${index + 1}. ${todo}`;
-          });
-          ctx.editMessageText(msg)
-          ctx.answerCallbackQuery({text: "todo eliminato",});
-        
-        })
-  });
+        // append to the message " select the todo to delete"
 
 
+        ctx.session.todo?.forEach((todo, index) => {
+            range
+                .text((index+1).toString(), (ctx) => {
+                    ctx.session.todo?.splice(index, 1)
+                    ctx.menu.update();
 
-  return range;
-})
+                    let msg = 'hai ' + ctx.session.todo?.length + ' cose da fare: \n'
+                    ctx.session.todo?.forEach((todo, index) => {
+                        msg += `\n${index + 1}. ${todo}`;
+                    });
+                    ctx.editMessageText(msg)
+                    ctx.answerCallbackQuery({ text: "todo eliminato", });
+
+                })
+        });
+
+
+
+
+        return range;
+    }).row()
+    .back("Go Back");
 
 
 
